bcf5d5135f58379e9c1b9e5c6410870f0d967293
Laurie <lauriemail@protonmail.com>
2025-02-24 13:32:25 +0700

    Remove TimelineEntry stuff

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 export * from './components/Button';
 export * from './components/ButtonGroup';
-export { CollapsibleTimeline } from './components/CollapsibleTimeline';
 export * from './components/GlobalStyle';
 export * from './components/LoadingOverlay';
 export * from './components/Spinner';
 export * from './components/TextInput';
 export * from './components/ThemeProvider';
-export { TimelineEntry } from './components/TimelineEntry';
 
 export * from './theme';
