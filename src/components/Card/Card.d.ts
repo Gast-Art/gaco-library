b72f361cb2e41188fa0620cfcaadb7533084fd30
littlefinger42 <lauriemail@protonmail.com>
2025-02-25 15:35:10 +0000

    Deploying to gh-pages from @ Gast-Art/gaco-library@5706e5f39720566feffad637d544b80a05dde7c2 🚀

--- src/components/Card/Card.d.ts
+++ src/components/Card/Card.d.ts
@@ -2,6 +2,8 @@ import { PropsWithChildren } from '../../../node_modules/react';
 export declare const CardSections: {
     Title: import('styled-components/dist/types').IStyledComponentBase<"web", import('styled-components').FastOmit<import('styled-components').FastOmit<import('../../../node_modules/react').DetailedHTMLProps<import('../../../node_modules/react').HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>, never>, never>> & string;
     Content: import('styled-components/dist/types').IStyledComponentBase<"web", import('styled-components').FastOmit<import('../../../node_modules/react').DetailedHTMLProps<import('../../../node_modules/react').HTMLAttributes<HTMLDivElement>, HTMLDivElement>, never>> & string;
+    LeftColumn: import('styled-components/dist/types').IStyledComponentBase<"web", import('styled-components').FastOmit<import('../../../node_modules/react').DetailedHTMLProps<import('../../../node_modules/react').HTMLAttributes<HTMLDivElement>, HTMLDivElement>, never>> & string;
+    RightColumn: import('styled-components/dist/types').IStyledComponentBase<"web", import('styled-components').FastOmit<import('../../../node_modules/react').DetailedHTMLProps<import('../../../node_modules/react').HTMLAttributes<HTMLDivElement>, HTMLDivElement>, never>> & string;
 };
 export declare const Card: ({ children, ...props }: PropsWithChildren) => import("react/jsx-runtime").JSX.Element;
 //# sourceMappingURL=Card.d.ts.map
