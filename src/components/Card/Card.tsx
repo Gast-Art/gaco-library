3b93645add5ca7f8469a083f203ea1db86b57f40
Laurie <lauriemail@protonmail.com>
2025-03-16 23:09:26 +0100

    Remove height 100% from Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,7 +8,6 @@ export const CardContainer = styled.div`
   background-color: ${({ theme }) => theme.colors.contentBg};
   box-shadow: ${({ theme }) => theme.shadows.card};
   width: 100%;
-  height: 100%;
 
   flex: 1;
   padding: 1rem 1.5rem;
