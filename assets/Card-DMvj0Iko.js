b7af9394efe66d781ea3c09f3f82130983991846
littlefinger42 <lauriemail@protonmail.com>
2025-03-16 22:10:24 +0000

    Deploying to gh-pages from @ Gast-Art/gaco-library@3b93645add5ca7f8469a083f203ea1db86b57f40 🚀

--- assets/Card-DMvj0Iko.js
+++ assets/Card-DMvj0Iko.js
@@ -22,12 +22,11 @@ import{j as m}from"./jsx-runtime-CLpGMVip.js";import{r as p}from"./index-CZMpeKR
     @media (min-width: ${s.xxl}) {
       ${t(...e)};
     }
-  `},g=i.div`
+  `},C=i.div`
   position: relative;
   background-color: ${({theme:e})=>e.colors.contentBg};
   box-shadow: ${({theme:e})=>e.shadows.card};
   width: 100%;
-  height: 100%;
 
   flex: 1;
   padding: 1rem 1.5rem;
@@ -37,11 +36,11 @@ import{j as m}from"./jsx-runtime-CLpGMVip.js";import{r as p}from"./index-CZMpeKR
   gap: 1rem;
 
   border-radius: ${({theme:e})=>e.sizes.borderRadius};
-`,C=i.div`
+`,g=i.div`
   display: flex;
   flex-direction: column;
   flex: 1;
-`,h=i.div`
+`,$=i.div`
   flex: 1;
   overflow-y: auto;
   max-height: 480px;
@@ -49,6 +48,6 @@ import{j as m}from"./jsx-runtime-CLpGMVip.js";import{r as p}from"./index-CZMpeKR
   ${c.md`
       max-height: none;
   `}
-`,$=i(u)`
+`,h=i(u)`
   margin-top: 0;
-`,y=i.div``,w=i.div``,n=Object.assign({},{Title:$,Content:h,LeftColumn:y,RightColumn:w}),v=({children:e,...x})=>{let o,a,l,d;const f=p.Children.toArray(e).filter(r=>typeof r=="string"?!0:p.isValidElement(r)?r.type===n.Title?(o=r,!1):r.type===n.Content?(a=r,!1):r.type===n.LeftColumn?(l=r,!1):r.type===n.RightColumn?(d=r,!1):!0:!1);return m.jsxs(g,{...x,children:[l,m.jsxs(C,{children:[o,a,f]}),d]})};v.__docgenInfo={description:"",methods:[],displayName:"Card",props:{className:{required:!1,tsType:{name:"string"},description:""},style:{required:!1,tsType:{name:"CSSProperties"},description:""},onClick:{required:!1,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""}},composes:["PropsWithChildren"]};export{v as C,n as a,g as b};
+`,y=i.div``,w=i.div``,n=Object.assign({},{Title:h,Content:$,LeftColumn:y,RightColumn:w}),v=({children:e,...x})=>{let o,a,l,d;const f=p.Children.toArray(e).filter(r=>typeof r=="string"?!0:p.isValidElement(r)?r.type===n.Title?(o=r,!1):r.type===n.Content?(a=r,!1):r.type===n.LeftColumn?(l=r,!1):r.type===n.RightColumn?(d=r,!1):!0:!1);return m.jsxs(C,{...x,children:[l,m.jsxs(g,{children:[o,a,f]}),d]})};v.__docgenInfo={description:"",methods:[],displayName:"Card",props:{className:{required:!1,tsType:{name:"string"},description:""},style:{required:!1,tsType:{name:"CSSProperties"},description:""},onClick:{required:!1,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""}},composes:["PropsWithChildren"]};export{v as C,n as a,C as b};
