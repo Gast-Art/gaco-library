998bb2f07ff0dfb9cebaa2f6dad51165bc14ec2e
littlefinger42 <lauriemail@protonmail.com>
2025-03-17 18:31:38 +0000

    Deploying to gh-pages from @ Gast-Art/gaco-library@11b8f0cd7b1784aa01cef27a7bf367bc54dd573c 🚀

--- src/components/Map/Map.stories.d.ts
+++ src/components/Map/Map.stories.d.ts
@@ -4,4 +4,5 @@ declare const meta: Meta<typeof Map>;
 export default meta;
 type Story = StoryObj<typeof Map>;
 export declare const Default: Story;
+export declare const WithMarker: Story;
 //# sourceMappingURL=Map.stories.d.ts.map
