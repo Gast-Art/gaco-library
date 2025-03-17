1ba0ae746d88ab43b94985cc377925ac293d7b8c
littlefinger42 <lauriemail@protonmail.com>
2025-03-17 17:54:08 +0000

    Deploying to gh-pages from @ Gast-Art/gaco-library@aba37ba0bff6604b201d1040f045519e6f2c5b22 🚀

--- assets/Typography-ThQ2eIx8.js
+++ assets/Typography-ThQ2eIx8.js
@@ -1,16 +1,14 @@
-import{d as t}from"./styled-components.browser.esm-BT5p091k.js";const i=t.h1`
+import{d as t}from"./styled-components.browser.esm-BT5p091k.js";const n=t.h1`
   font-size: ${({theme:o})=>o.sizes.fonts.xxxl};
   font-family: 'Lora', serif;
   font-weight: bold;
 
   margin-bottom: 1rem;
-`,n=t.h1`
-  font-family: 'Lora', serif;
+`,e=t.h1`
   font-size: ${({theme:o})=>o.sizes.fonts.xxl};
   font-weight: bold;
   margin-bottom: 1rem;
-`,e=t.h2`
-  font-family: 'Lora', serif;
+`,i=t.h2`
   font-size: ${({theme:o})=>o.sizes.fonts.xl};
   font-weight: bold;
   margin-bottom: 0.75rem;
@@ -24,4 +22,4 @@ import{d as t}from"./styled-components.browser.esm-BT5p091k.js";const i=t.h1`
   margin-bottom: 0.5rem;
 `;t.small`
   font-size: ${({theme:o})=>o.sizes.fonts.xxs};
-`;export{n as H,i as P,e as a,f as b,m as c};
+`;export{e as H,n as P,i as a,f as b,m as c};
