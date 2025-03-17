39b0152ef737de98f8a0f3cffd8d32d878664fb0
littlefinger42 <lauriemail@protonmail.com>
2025-03-17 15:59:15 +0000

    Deploying to gh-pages from @ Gast-Art/gaco-library@758ab17fb45c6fb419f4dd40ce9885cf3ad9ca5f 🚀

---
+++ assets/Typography-B3MKvc-A.js
@@ -0,0 +1,25 @@
+import{d as t}from"./styled-components.browser.esm-BT5p091k.js";const i=t.h1`
+  font-size: ${({theme:o})=>o.sizes.fonts.xxxl};
+  font-family: 'Lora', serif;
+  font-weight: bold;
+`,n=t.h1`
+  font-family: 'Lora', serif;
+  font-size: ${({theme:o})=>o.sizes.fonts.xxl};
+  font-weight: bold;
+  margin-bottom: 1rem;
+`,e=t.h2`
+  font-family: 'Lora', serif;
+  font-size: ${({theme:o})=>o.sizes.fonts.xl};
+  font-weight: bold;
+  margin-bottom: 0.75rem;
+`,f=t.h3`
+  font-size: ${({theme:o})=>o.sizes.fonts.lg};
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+`,a=t.h4`
+  font-size: ${({theme:o})=>o.sizes.fonts.md};
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+`;t.small`
+  font-size: ${({theme:o})=>o.sizes.fonts.xxs};
+`;export{n as H,i as P,e as a,f as b,a as c};
