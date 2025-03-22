173d34a0e456e0a9606f3eda7d27b268d6448082
Laurie <lauriemail@protonmail.com>
2025-03-22 16:35:09 +0100

    Add size prop to Card in CardList

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,10 +1,11 @@
 import { PropsWithChildren, ReactNode } from 'react';
 import styled from 'styled-components';
-import { CardContainer, Card as CardRoot, CardSections } from '../Card';
+import { CardContainer, Card as CardRoot, CardSections, CardSize } from '../Card';
 
 export interface CardProps extends PropsWithChildren {
   className?: string;
   id: string | number;
+  size?: CardSize;
   title?: ReactNode;
   // subtitle?: string;
   content?: ReactNode;
@@ -15,7 +16,7 @@ export interface CardProps extends PropsWithChildren {
 
 const Card = (card: CardProps) => {
   return (
-    <CardRoot onClick={card.onClick} className={card.className}>
+    <CardRoot onClick={card.onClick} className={card.className} size={card.size}>
       {card.title && <CardSections.Title>{card.title}</CardSections.Title>}
       {/* {card.subtitle && <CardSubtitle>{card.subtitle}</CardSubtitle>} */}
       {card.content && <CardSections.Content>{card.content}</CardSections.Content>}
