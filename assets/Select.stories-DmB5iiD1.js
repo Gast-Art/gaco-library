fc1396150a3aa4fe4652d392974de46848fa1765
littlefinger42 <lauriemail@protonmail.com>
2025-05-05 12:17:55 +0000

    Deploying to gh-pages from @ Gast-Art/gaco-library@92daadf41a292535e84ef342a123f473b2c5680d 🚀

---
+++ assets/Select.stories-DmB5iiD1.js
@@ -0,0 +1,40 @@
+import{j as t}from"./jsx-runtime-CLpGMVip.js";import{r as O}from"./index-CR__hKHy.js";import{S as u}from"./Select-C8r1rpLt.js";import"./react-select.esm-BxwBLSRO.js";import"./styles-pgyJtqjm.js";import"./createLucideIcon-BOMFEwrg.js";import"./index-D2e_E8nu.js";import"./index-CtmzRm2p.js";import"./DropdownMenu-5ajHuE75.js";import"./index-bxkApRut.js";import"./styled-components.browser.esm-DnH8uv-2.js";import"./circle-alert-C-cAHhHG.js";const w={title:"Atoms/Select",component:u,decorators:[o=>t.jsx("div",{style:{width:"320px"},children:t.jsx(o,{})})]},m=o=>{const[c,b]=O.useState();return t.jsx(u,{...o,value:c,onChange:d=>b(d)})},e={render:m,args:{options:[{value:"1",label:"Option 1"},{value:"2",label:"Option 2"},{value:"3",label:"Option 3"}],label:"Select an option"}},n={render:m,args:{options:[{label:"Group 1",options:[{value:"1",label:"Option 1"},{value:"2",label:"Option 2"}]},{label:"Group 2",options:[{value:"3",label:"Option 3"},{value:"4",label:"Option 4"}]}],label:"Select an option"}};var a,l,r;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    options: [{
+      value: '1',
+      label: 'Option 1'
+    }, {
+      value: '2',
+      label: 'Option 2'
+    }, {
+      value: '3',
+      label: 'Option 3'
+    }],
+    label: 'Select an option'
+  }
+}`,...(r=(l=e.parameters)==null?void 0:l.docs)==null?void 0:r.source}}};var p,s,i;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    options: [{
+      label: 'Group 1',
+      options: [{
+        value: '1',
+        label: 'Option 1'
+      }, {
+        value: '2',
+        label: 'Option 2'
+      }]
+    }, {
+      label: 'Group 2',
+      options: [{
+        value: '3',
+        label: 'Option 3'
+      }, {
+        value: '4',
+        label: 'Option 4'
+      }]
+    }],
+    label: 'Select an option'
+  }
+}`,...(i=(s=n.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};const y=["Default","GroupedOptions"];export{e as Default,n as GroupedOptions,y as __namedExportsOrder,w as default};
