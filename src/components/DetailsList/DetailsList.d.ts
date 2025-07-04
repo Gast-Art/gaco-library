ac6276e8985d707a2997959dce8adfcaf235fffe
littlefinger42 <lauriemail@protonmail.com>
2025-07-04 11:07:47 +0000

    Deploying to gh-pages from @ Gast-Art/gaco-library@345e5a039bb249bcbf09ae4b50de1bbfb288b5ef 🚀

--- src/components/DetailsList/DetailsList.d.ts
+++ src/components/DetailsList/DetailsList.d.ts
@@ -1,2 +1,4 @@
-export declare const DetailsList: import('styled-components/dist/types').IStyledComponentBase<"web", import('styled-components').FastOmit<import('../../../node_modules/react').DetailedHTMLProps<import('../../../node_modules/react').HTMLAttributes<HTMLDListElement>, HTMLDListElement>, never>> & string;
+export declare const DetailsList: import('styled-components/dist/types').IStyledComponentBase<"web", import('styled-components/dist/types').Substitute<import('../../../node_modules/react').DetailedHTMLProps<import('../../../node_modules/react').HTMLAttributes<HTMLDListElement>, HTMLDListElement>, {
+    $hideBorder?: boolean;
+}>> & string;
 //# sourceMappingURL=DetailsList.d.ts.map
