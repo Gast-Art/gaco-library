47569ba393b6da8461c55663210ec10e77d4cb79
Laurie <lauriemail@protonmail.com>
2025-07-04 13:00:39 +0200

    CHange width of detaillist to be auto

--- src/components/DetailsList/DetailsList.tsx
+++ src/components/DetailsList/DetailsList.tsx
@@ -2,26 +2,22 @@ import styled from 'styled-components';
 
 export const DetailsList = styled.dl`
   display: grid;
-  grid-template-columns: repeat(2, minmax(0, 1fr));
+  grid-template-columns: auto 1fr;
   grid-row-gap: 0.25rem;
   margin: 0;
   padding: 0;
   list-style: none;
+  width: max-content;
 
   dt,
   dd {
     margin: 0;
-    padding: 0 0 0.125rem;
+    padding: 0 0.5rem 0.125rem;
     border-bottom: 1px ${({ theme }) => theme.colors.mutedBg} solid;
   }
 
   dd {
     font-weight: 500;
-    padding-left: 0.5rem;
-  }
-
-  dt {
-    padding: 0;
   }
 
   dt:last-of-type,
