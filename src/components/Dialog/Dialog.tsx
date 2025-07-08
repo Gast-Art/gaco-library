1a6e2cc63134d88169c183e574f1770efeb74405
Laurie <lauriemail@protonmail.com>
2025-07-08 13:57:46 +0200

    Attempt to fi dialog zIndex

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -15,7 +15,7 @@ const DialogOverlay = styled(DialogRoot.Overlay)`
   right: 0;
   bottom: 0;
   background-color: rgba(0, 0, 0, 0.3);
-  z-index: ${({ theme }) => theme.zIndices.dialogOverlay};
+  z-index: 6000;
 `;
 
 const DialogContent = styled(DialogRoot.Content)`
