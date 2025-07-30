27c0ba4253905ca49d14e603ee478e00893d8140
littlefinger42 <lauriemail@protonmail.com>
2025-07-30 15:57:44 +0000

    Deploying to gh-pages from @ Gast-Art/gaco-library@20e2d2fd6a9407edd5ab33b4bcd9009d523be015 🚀

--- src/components/TooltipChart/TooltipChart.d.ts
+++ src/components/TooltipChart/TooltipChart.d.ts
@@ -3,8 +3,9 @@ import { TooltipProps } from 'recharts';
 import { NameType, ValueType } from 'recharts/types/component/DefaultTooltipContent';
 interface TooltipChartProps extends TooltipProps<ValueType, NameType> {
     tooltipValueFormatter?: (value: any) => string;
+    tooltipLabelFormatter?: (label: string) => string;
     children?: ReactNode;
 }
-declare const TooltipChart: ({ active, payload, label, tooltipValueFormatter, children }: TooltipChartProps) => import("react/jsx-runtime").JSX.Element | null;
+declare const TooltipChart: ({ active, payload, label, tooltipLabelFormatter, tooltipValueFormatter, children, }: TooltipChartProps) => import("react/jsx-runtime").JSX.Element | null;
 export { TooltipChart };
 //# sourceMappingURL=TooltipChart.d.ts.map
