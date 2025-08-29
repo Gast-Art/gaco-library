a8692a5301b8326d755b2e7ae477155c23f6d191
littlefinger42 <lauriemail@protonmail.com>
2025-08-29 15:42:02 +0000

    Deploying to gh-pages from @ Gast-Art/gaco-library@0aafda5cf28f6e53a163c12625194b85fa239b25 🚀

--- src/components/GlobalStyle/GlobalStyle.d.ts
+++ src/components/GlobalStyle/GlobalStyle.d.ts
@@ -1,5 +1,5 @@
 import { DefaultTheme } from 'styled-components/dist/types';
 export declare const GlobalStyle: import('../../../node_modules/react').NamedExoticComponent<import('styled-components').ExecutionProps & {
-    theme: DefaultTheme;
+    theme?: DefaultTheme;
 }>;
 //# sourceMappingURL=GlobalStyle.d.ts.map
