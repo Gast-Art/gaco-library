4c3b7ddf9638acf1418083c2e2c5b930afd26463
Laurie <lauriemail@protonmail.com>
2025-08-29 13:33:43 +0200

    Improve theme declarations file for typescript to reduce manual strict typing

--- styled.d.ts
+++ styled.d.ts
@@ -1,21 +1,8 @@
+import { theme } from 'src/theme';
 import 'styled-components';
 
 declare module 'styled-components' {
-  export interface DefaultTheme {
-    colors: {
-      [key: string]: string;
-    };
-    shadows: {
-      [key: string]: string;
-    };
-    zIndices: {
-      [key: string]: number;
-    };
-    fonts: {
-      [key: string]: string;
-    };
-    sizes: {
-      [key: string]: any;
-    };
-  }
+  type MyTheme = typeof theme;
+
+  export interface DefaultTheme extends MyTheme {}
 }
